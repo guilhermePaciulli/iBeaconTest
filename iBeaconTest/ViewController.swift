3d89bc0527c99111fdb0461511e3d4f68473e262
DaviBrancol <daviduarbrancol@gmail.com>
2017-05-10 17:30:45 -0300

    Nicely done

--- iBeaconTest/ViewController.swift
+++ iBeaconTest/ViewController.swift
@@ -13,15 +13,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     
-    var beaconsDistances = {"Beacon1": [], "Beacon2": [], "Beacon3": []}
+    @IBOutlet weak var distancLabel: UILabel!
+    @IBOutlet weak var distancLabel2: UILabel!
     
-    var b1: Double?
-    
-    var b2: Double?
-
-    var b3: Double?
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
@@ -38,30 +32,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    @IBOutlet weak var myPosImage: UIImageView!
+    @IBAction func calculateDistance(_ sender: Any) {
+    
+        
+    }
+    
+    func calculateDistance2(_ d1: Double,_ d2: Double,_ d3: Double) {
+    
+        let point1 = Point(position: (xCoord: 0, yCoord: 0, zCoord: nil), distance: d1)
+        let point2 = Point(position: (xCoord: 0, yCoord: 9, zCoord: nil), distance: d2)
+        let point3 = Point(position: (xCoord: 5, yCoord: 20, zCoord: nil), distance: d3)
+        
+        let myLoc = Trilaterization.trilateration(point1: point1, point2: point2, point3: point3)
+        
+        myPosImage.frame.origin.x = myPosImage.frame.size.width * CGFloat(myLoc[1])
+        myPosImage.frame.origin.y = myPosImage.frame.size.height * CGFloat(myLoc[0])
+    
+    }
+    
+    
     func startScanning() {
         let uuid = UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
-        let beaconRegion1 = CLBeaconRegion(proximityUUID: uuid, major: 30969, minor: 18337, identifier: "Beacon1")
-        let beaconRegion2 = CLBeaconRegion(proximityUUID: uuid, major: 33119, minor: 33895, identifier: "Beacon2")
-        let beaconRegion3 = CLBeaconRegion(proximityUUID: uuid, major: 35079, minor: 46811, identifier: "Beacon3")
-
-        locationManager.startMonitoring(for: beaconRegion1)
-        locationManager.startRangingBeacons(in: beaconRegion1)
         
-        locationManager.startMonitoring(for: beaconRegion2)
-        locationManager.startRangingBeacons(in: beaconRegion2)
+        let beaconRegion3 = CLBeaconRegion.init(proximityUUID: uuid, identifier: "aa")
+        //(proximityUUID: uuid, major: 35079, minor: 46811, identifier: "Beacon3")
+
         
         locationManager.startMonitoring(for: beaconRegion3)
         locationManager.startRangingBeacons(in: beaconRegion3)
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        for beacon in beacons {
-            if beacon.major == 33119 && beacon.minor == 33895 {
-                b1 = beacon.accuracy
-            } else if beacon.major == 30969 && beacon.minor == 18337 {
-                b3 = beacon.accuracy
-            } else if beacon.major == 35079 && beacon.minor == 46811 {
-                b2 = beacon.accuracy
+        print(beacons.count)
+        if beacons.count > 2 {
+            for beacon in beacons {
+                var d1: Double = 0
+                var d2: Double = 0
+                var d3: Double = 0
+                if beacon.major == 33119 && beacon.minor == 33895 {
+                    d1 = beacon.accuracy
+                } else if beacon.major == 30969 && beacon.minor == 18337 {
+                    d2 = beacon.accuracy
+                } else if beacon.major == 35079 && beacon.minor == 46811 {
+                    d3 = beacon.accuracy
+                }
+                print("\(d1)/\(d2)/\(d3)")
+                calculateDistance2(d1,d2,d3)
             }
         }
     }
